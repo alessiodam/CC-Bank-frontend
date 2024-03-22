14d443204d09cb1f6ba8fd83b5ce85434d675cae
TKB Studios <69647028+tkbstudios@users.noreply.github.com>
2024-03-22 18:53:48 +0100

    start with login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,18 +1,11 @@
 import React from 'react';
+import { Input } from "@/components/ui/input"
 import './page.css';
 
 const Page = () => {
     return (
         <div className="container">
-            <h1>Login</h1>
-            <p>Enter your username and password to login.</p>
-            <form>
-                <label htmlFor="username">Username</label>
-                <input type="text" id="username" name="username" />
-                <label htmlFor="password">Password</label>
-                <input type="password" id="password" name="password" />
-                <button type="submit">Login</button>
-            </form>
+            <Input type="email" placeholder="Email" />
         </div>
     );
 };
