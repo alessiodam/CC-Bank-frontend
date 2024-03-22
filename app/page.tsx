0937ffcf3625891a0d51ad2c72820605fccc8014
TKB Studios <69647028+tkbstudios@users.noreply.github.com>
2024-03-22 18:25:15 +0100

    nextjs

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <Container>
+      <Heading as="h1">Welcome to our website!</Heading>
+      <Text>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae
+        euismod felis.
+      </Text>
+      <Card>
+        <Button variant="primary">Get Started</Button>
+      </Card>
+    </Container>
+  );
+}
