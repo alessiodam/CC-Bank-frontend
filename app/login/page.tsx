7c685e4c374cb708cd0b87ff4ad07e3cc6423d37
TKB Studios <69647028+tkbstudios@users.noreply.github.com>
2024-03-23 09:00:34 +0100

    basic login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,62 @@
-import React from 'react';
+"use client";
+import React, { useState } from 'react';
 import { Input } from "@/components/ui/input"
 import './page.css';
+import { Button } from '@/components/ui/button';
+import { Terminal } from "lucide-react"
+
+import {
+    Alert,
+    AlertDescription,
+    AlertTitle,
+} from "@/components/ui/alert"
 
 const Page = () => {
+    const [username, setUsername] = useState("");
+    const [pin, setPin] = useState("");
+
+    const handleLogin = async () => {
+        try {
+            const response = await fetch("http://localhost:8123/api/v1/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    username,
+                    pin: parseInt(pin),
+                }),
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                const { session_token } = data;
+
+                document.cookie = `session_token=${session_token}; path=/`;
+                console.log("Login successful");
+                window.location.href = '/dashboard';
+            } else {
+                console.error("Login failed");
+                
+            }
+        } catch (error) {
+            console.error("An error occurred during login:", error);
+        }
+    };
+
     return (
         <div className="container">
-            <Input type="email" placeholder="Email" />
+            <p className="login-title">Login</p>
+            <Alert>
+                <Terminal className="h-4 w-4" />
+                <AlertTitle>Heads up!</AlertTitle>
+                <AlertDescription>
+                    This is still very early and might not work as expected.
+                </AlertDescription>
+            </Alert>
+            <Input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+            <Input type="password" placeholder="PIN" value={pin} onChange={(e) => setPin(e.target.value)} />
+            <Button className="w-full" onClick={handleLogin}>Login</Button>
         </div>
     );
 };
