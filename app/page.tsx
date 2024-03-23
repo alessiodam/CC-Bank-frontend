cc2f2e95288973ac00406b83700dbeddd4d4de67
TKB Studios <69647028+tkbstudios@users.noreply.github.com>
2024-03-23 14:39:21 +0100

    escape '

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ const Page = () => {
             <h1>Bank Of ComputerCraft</h1>
             <p>Welcome to the Bank Of ComputerCraft, your trusted source for advanced computer services. It is a bank system synced across all servers, allowing users to access their coins and funds seamlessly across different servers.</p>
             <p>To install Bank Of ComputerCraft into an Advanced Computer, use the following one-liner:</p>
-            <p>Bank Of Computercraft has auto-updating, you won't need to do anything to update it.</p>
+            <p>Bank Of Computercraft has auto-updating, you won&apos;t need to do anything to update it.</p>
             <div className="code-box">
                 <p>pastebin run RN5J9wvi</p>
             </div>
